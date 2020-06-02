c7c9fb01d0f3f441022b949f682238ae936b350f
reznikovkg <reznikov.kosta@yandex.ru>
2020-06-02 16:58:32 +0300

    new

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -31,6 +31,8 @@ import Table from '@ckeditor/ckeditor5-table/src/table';
 import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
 import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation';
 
+import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment';
+
 export default class ClassicEditor extends ClassicEditorDecoupled {}
 
 // Plugins to include in the build.
@@ -57,7 +59,9 @@ ClassicEditor.builtinPlugins = [
 	PasteFromOffice,
 	Table,
 	TableToolbar,
-	TextTransformation
+	TextTransformation,
+
+	Alignment
 ];
 
 // Editor configuration.
@@ -65,6 +69,7 @@ ClassicEditor.defaultConfig = {
 	toolbar: {
 		items: [
 			'heading',
+			'alignment',
 			'|',
 			'bold',
 			'italic',
